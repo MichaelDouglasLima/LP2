b46b4ab6312e9dfb57b894b2a4e5f2fe4dc49fc2
MichaelDouglasLima <110948999+MichaelDouglasLima@users.noreply.github.com>
2022-11-26 20:39:11 -0300

    Add files via upload

---
+++ BackupProjetoLP2/PCopa0030482211041/PCopa0030482211041/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace PCopa0030482211041
+{
+    public partial class frmPrincipal : Form
+    {
+        public static SqlConnection conexao;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // aqui a conexão vai depende da sua máquina da escola ou particular 
+                conexao = new SqlConnection("Data Source=Apolo;Initial Catalog=LP2;Integrated Security=False;User ID=BD2211041;Password=MichaelBD99");
+                conexao.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Erro de banco de dados =/" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Outros Erros =/" + ex.Message);
+            }
+        }
+    }
+}
